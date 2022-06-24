0148b94b064fe0871ff8f01c27a8cbbfad31d22c
Edward <asantanadurango@gmail.com>
2022-06-24 12:33:57 -0500

    se crea interfaz index

---
+++ src/controllers/controladorindex.js
@@ -0,0 +1,138 @@
+let productos = [producto1, producto2, producto3, producto4, producto5, producto6, producto7, producto8 ]
+
+let producto1 = {
+    nombre : 'Reloj',
+    precio : '45200000',
+    cantidad : '1',
+    foto : 'url',
+    descripcion : 'Reloj Cartier de mujer'
+}
+
+let producto2 = {
+    nombre : 'Reloj',
+    precio : '45200000',
+    cantidad : '1',
+    foto : 'url',
+    descripcion : 'Reloj Cartier de mujer'
+}
+
+let producto3 = {
+    nombre : 'Reloj',
+    precio : '45200000',
+    cantidad : '1',
+    foto : 'url',
+    descripcion : 'Reloj Cartier de mujer'
+}
+
+let producto4 = {
+    nombre : 'Reloj',
+    precio : '45200000',
+    cantidad : '1',
+    foto : 'url',
+    descripcion : 'Reloj Cartier de mujer'
+}
+
+let producto5 = {
+    nombre : 'Reloj',
+    precio : '45200000',
+    cantidad : '1',
+    foto : 'url',
+    descripcion : 'Reloj Cartier de mujer'
+}
+
+let producto6 = {
+    nombre : 'Reloj',
+    precio : '45200000',
+    cantidad : '1',
+    foto : 'url',
+    descripcion : 'Reloj Cartier de mujer'
+}
+
+let producto7 = {
+    nombre : 'Reloj',
+    precio : '45200000',
+    cantidad : '1',
+    foto : 'url',
+    descripcion : 'Reloj Cartier de mujer'
+}
+
+let producto8 = {
+    nombre : 'Reloj',
+    precio : '45200000',
+    cantidad : '1',
+    foto : 'url',
+    descripcion : 'Reloj Cartier de mujer'
+}
+
+
+let producto9 = {
+    nombre : 'Reloj',
+    precio : '45200000',
+    cantidad : '1',
+    foto : 'url',
+    descripcion : 'Reloj Cartier de mujer'
+}
+
+
+let producto10 = {
+    nombre : 'Reloj',
+    precio : '45200000',
+    cantidad : '1',
+    foto : 'url',
+    descripcion : 'Reloj Cartier de mujer'
+}
+
+
+let producto11 = {
+    nombre : 'Reloj',
+    precio : '45200000',
+    cantidad : '1',
+    foto : 'url',
+    descripcion : 'Reloj Cartier de mujer'
+}
+
+
+let producto12 = {
+    nombre : 'Reloj',
+    precio : '45200000',
+    cantidad : '1',
+    foto : 'url',
+    descripcion : 'Reloj Cartier de mujer'
+}
+
+
+let producto13 = {
+    nombre : 'Reloj',
+    precio : '45200000',
+    cantidad : '1',
+    foto : 'url',
+    descripcion : 'Reloj Cartier de mujer'
+}
+
+
+let producto14 = {
+    nombre : 'Reloj',
+    precio : '45200000',
+    cantidad : '1',
+    foto : 'url',
+    descripcion : 'Reloj Cartier de mujer'
+}
+
+
+let producto15 = {
+    nombre : 'Reloj',
+    precio : '45200000',
+    cantidad : '1',
+    foto : 'url',
+    descripcion : 'Reloj Cartier de mujer'
+}
+
+
+let producto16 = {
+    nombre : 'Reloj',
+    precio : '45200000',
+    cantidad : '1',
+    foto : 'url',
+    descripcion : 'Reloj Cartier de mujer'
+}
+
---
+++ src/controllers/index.js
@@ -0,0 +1,16 @@
+let poliitico = {
+    nombre: 'alvaro',
+    poder: 'dinero',
+    edad : '64',
+}
+
+//controlando etiquetas 
+//almacenar la variable
+let titulo = document.getElementById("titulo")
+let imagen =  document.getElementById("foto")
+
+//identifico que quiero controlar
+titulo.textContent = "chao"
+
+// cambiar la fuente 
+imagen.src= "https://placeimg.com/640/480/any"
